758d7f2d8d412f7707ac992414b1761561ec87f4
yogisumarga <38517044+yogisumarga@users.noreply.github.com>
2021-08-22 19:05:02 +0700

    Fourth commit

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Everyone in this world");  
+     System.out.println("Hello Everyone in the world of fun");  
     }  
 }  
